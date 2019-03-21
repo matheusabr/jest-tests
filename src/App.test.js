59b50696fc04221ba0a8cb66b52be682d7c6aa5c
Matheus Abreu <mabreu@PDM-00870.local>
2019-03-21 10:21:52 +0000

    task: add simple numerical tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+  ReactDOM.unmountComponentAtNode(div)
+})
+
+test('simple numerical tests', () => {
+  expect(4).toBe(4)
+  expect(4).toBeGreaterThan(0)
+  expect(4).toBeLessThan(10)
+  expect(4).toEqual(4)
+  expect(4.215).toBeCloseTo(4.21)
+})
