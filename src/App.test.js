37f544f5dc23e6f920e5f6c6010adb856b66ebd7
Matheus Abreu <mabreu@PDM-00870.local>
2019-03-21 11:03:05 +0000

    task: add simple string, objects and arrays tests

--- src/App.test.js
+++ src/App.test.js
@@ -15,3 +15,28 @@ test('simple numerical tests', () => {
   expect(4).toEqual(4)
   expect(4.215).toBeCloseTo(4.21)
 })
+
+test('simple string, objects and arrays tests', () => {
+  expect('Matheus Abreu').toMatch(/Matheus/)
+  expect('Matheus Abreu').not.toMatch(/Paul/)
+  const arrList = [
+    {
+      id: 1,
+      isActive: true,
+      name: 'MiBr',
+      country: 'Brazil'
+    },
+    {
+      id: 2,
+      isActive: false,
+      name: 'Astralis',
+      country: 'Denmark'
+    }
+  ]
+  const obj = arrList[0]
+  expect(obj).toHaveProperty('id')
+  expect(obj).toHaveProperty('name', 'MiBr')
+  expect(obj).toMatchObject({ name: 'MiBr' })
+  expect(obj.isActive).toBeTruthy()
+  expect(arrList).toHaveLength(2)
+})
